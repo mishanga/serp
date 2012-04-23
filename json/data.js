9c12dca2ba1af30b6e3e20fbb1e6b0194f1065c6
Mikhail Troshev <mishanga@yandex-team.ru>
2012-04-23 18:30:08 +0400

    Data ctx example

---
+++ json/data.js
@@ -0,0 +1,45 @@
+({
+    "docs": [
+        {
+            "title": "Free Porn Videos & Sex Movies - Porno, XXX, Porn Tube and Pussy Porn",
+            "url": "http://www.pornhub.com/",
+            "text": [ "We offer streaming porn videos, downloadable XXX DVDs, photo albums, and the number 1 free sex community on the net. We’re always working towards adding more features that will keep your porno..." ]
+        }, {
+            "title": "ГИГ ПОРНО - Порно Видео - смотреть еще бесплатное онлайн секс видео...",
+            "url": "http://gigporno.ru/",
+            "text": [ "Располагайтесь и смотрите новое порно онлайн что приготовил вам ГИГПОРНО сегодня ", "Порно Видео - смотреть еще бесплатное онлайн секс видео на Gig Porno." ]
+        }, {
+            "title": "Porn Videos, Sex, XXX, Free Porn Tube - YouPorn",
+            "url": "http://www.youporn.com/",
+            "text": [ "You Porn.com is the largest porn site with the best variety of free full length porn videos - XXX, sex, porno, hardcore sex tube and more." ]
+        }, {
+            "title": "xHamster's Free Porn Videos",
+            "url": "http://www.xhamster.com/",
+            "text": [ "Biggest collection of free amateur porn videos! Tons of well organized movies are waiting for you." ]
+        }, {
+            "title": "Free Porn Sex Videos - Redtube - XXX Movies - Home of Porno",
+            "url": "http://www.redtube.com/",
+            "text": [ "RedTube brings you new free porn videos every day. Watch great xxx sex videos and pornos at the best free pussy and porn tube site on the web." ]
+        }, {
+            "title": "Порно фото галереи • Новое порно фото 1 - 48",
+            "url": "http://www.youporn.ru/photos/",
+            "text": [ "приколы Порно с неграми и мулатками Пьяный секс и Порно вечеринки Русское порно Секс и Порно Скрытая камера и Подглядывания Сперма Толстушки Транссексуалы Фетиш и..." ]
+        }, {
+            "title": "Порно онлайн, порно видео онлайн, смотреть порно онлайн",
+            "url": "http://xpornon.com/",
+            "text": [ "Добро пожаловать на порно онлайн портал xPornON.com. Здесь Вы окунётесь в море отборного порно, узнаете все тайны и хитрости секса." ]
+        }, {
+            "title": "Free Porn Tube Movies, Sex Videos, Porno Clips & XXX Pics",
+            "url": "http://www.keezmovies.com/",
+            "text": [ "Hot free porn tube movies, XXX amateur sex videos, & upload Your Own porno movies. See the hottest pornstars having sex and see it at the best porntube Keezmovies.com." ]
+        }, {
+            "title": "Free Porn Videos, Porn Tube, Free Porn, Free Porno Movies, Free Sex",
+            "url": "http://www.tnaflix.com/",
+            "text": []
+        }, {
+            "title": "The Best Free Porn Tube, XXX, Porno and Sex Videos - PORN.COM",
+            "url": "http://www.porn.com/",
+            "text": [ "Free Porn Videos Being Watched. 24:16 Hot Black Babe Shows Off Booty And Puts Her... 87%", "Our PORN.COM team works hard to keep you hard with constant, fresh porno vids posted on a DAILY..." ]
+        }
+    ]
+})
